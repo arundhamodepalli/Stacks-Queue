22501efb358cea8b3f05f6f6a128356be70cc9a6
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-03-23 15:36:56 +0530

    welcome to stacks and queus

---
+++ Stack_Queue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stack_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to stacks and queues");
+        }
+    }
+}
