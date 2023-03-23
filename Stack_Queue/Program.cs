9581834c6a95f74e6e21cf85b53cf9f86229556d
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-03-23 15:43:20 +0530

    Create stacks in stacks and queus

--- Stack_Queue/Program.cs
+++ Stack_Queue/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Stack_Queue
+﻿using static Stack_Queue.CreateStack;
+
+namespace Stack_Queue
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to stacks and queues");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine(stack.Peek());
         }
     }
 }
